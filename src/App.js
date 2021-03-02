ee68e93479973a7a61df8d0fb9811868e8516552
ganjanggansik <kjskor@gmail.com>
2021-03-02 10:27:49 +0900

    이미지추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-function Food({fav}) {
+function Food({name ,picture}) {
   
-  return <h5>{fav}</h5>;
+  return (
+    <div>
+      <h2>I LIKE {name}</h2>
+      <img src={picture} />
+    </div>
+  );
   //return <h5>I LIKE {props.fav}</h5>;
 
   //console.log(props);
@@ -10,13 +15,26 @@ function Food({fav}) {
   //return <h6>I LOVE</h6>;
 }
 
+const foodILike = [
+  {
+    name:'kimchi',
+    image:'https://m.jnmall.kr/web/product/big/201904/609ee5a2a4bea63000745b65a63f064a.png',
+  },
+  {
+    name:'ramen',
+    image:'http://res.heraldm.com/phpwas/restmb_idxmake.php?idx=507&simg=/content/image/2020/05/23/20200523000093_0.jpg',
+  },
+];
+
+
 function App() {
   //return <div className="App"/>;
   //return <div>hello wor</div>;
-  return (<div>
-    <Food fav="kimchi"></Food>
-    <Food fav="noodle"></Food>
-  </div>
+  return (
+    <div>
+      {foodILike.map(dish => ( 
+        <Food name={dish.name} picture={dish.image} ></Food>))}
+    </div>
   );
   //return <Food fav="kimchi" somthing={true} papapa ={['banana',1,2,3,4,true]}></Food>
 }
